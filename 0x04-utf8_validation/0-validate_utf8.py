b31df711e8262dd4abafb3f5fd7b9f0df45f2a82
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-08-22 22:28:40 +0300

    0. UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,20 +8,19 @@ def validUTF8(data):
     num_of_bytes = 0
 
     for value in data:
-        if num_of_bytes > 0:
-            if value >> 6 != 0b10:
+        value_binary = bin(value).replace("0b", "").rjust(8, "0")[-8:]
+        if num_of_bytes == 0:
+            if value_binary.startswith("110"):
+                num_of_bytes = 1
+            if value_binary.startswith("1110"):
+                num_of_bytes = 2
+            if value_binary.startswith("11110"):
+                num_of_bytes = 3
+            if value_binary.startswith("10"):
+                return False
+        else:
+            if not value_binary.startswith("10"):
                 return False
             num_of_bytes -= 1
-        else:
-            if value >> 7 == 0:
-                num_of_bytes = 0
-            elif value >> 5 == 0b110:
-                num_of_bytes = 1
-            elif value >> 4 == 0b1110:
-                num_of_bytes = 2
-            elif value >> 3 == 0b11110:
-                num_of_bytes = 3
-            else:
-                return False
 
     return num_of_bytes == 0
