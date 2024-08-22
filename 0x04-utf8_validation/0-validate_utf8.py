c11adcc8e6434fcf78cb5aa86d73dc6f2bf67f2e
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-08-22 20:43:54 +0300

    0. UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""0-validate_utf8"""
+from typing import List
+
+
+def validUTF8(data: List[int]) -> bool:
+    """validUTF8 function"""
+
+    num_of_bytes = 0
+
+    for value in data:
+        if num_of_bytes > 0:
+            if value >> 6 != 0b10:
+                return False
+            num_of_bytes -= 1
+        else:
+            if value >> 7 == 0:
+                num_of_bytes = 0
+            elif value >> 5 == 0b110:
+                num_of_bytes = 1
+            elif value >> 4 == 0b1110:
+                num_of_bytes = 2
+            elif value >> 3 == 0b11110:
+                num_of_bytes = 3
+            else:
+                return False
+
+    return num_of_bytes == 0
