8e487ebdc6d60ccbde38fed023797edd70bfc19f
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-09-20 00:19:44 +0300

    0. Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,12 +6,29 @@ def makeChange(coins, total):
     """Make Change Function"""
     if total <= 0:
         return 0
+    if coins == [] or coins is None:
+        return -1
+    try:
+        coins.index(total)
+        return 1
+    except ValueError:
+        pass
 
-    monc = [float("inf")] * (total + 1)
-    monc[0] = 0
-
-    for coin in coins:
-        for i in range(coin, total + 1):
-            monc[i] = min(monc[i], monc[i - coin] + 1)
-
-    return monc[total] if monc[total] != float("inf") else -1
+    coins.sort(reverse=True)
+    cc = 0
+    for i in coins:
+        if total % i == 0:
+            cc += int(total / i)
+            return cc
+        if total - i >= 0:
+            if int(total / i) > 1:
+                cc += int(total / i)
+                total = total % i
+            else:
+                cc += 1
+                total -= i
+                if total == 0:
+                    break
+    if total > 0:
+        return -1
+    return cc
